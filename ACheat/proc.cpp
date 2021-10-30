f6922ab7d189cde905f4cc88100078b2be93a59b
Emir Gocturk <emirgoppa@gmail.com>
2021-10-30 12:38:23 +0200

    boilerplate for proc

--- ACheat/proc.cpp
+++ ACheat/proc.cpp
@@ -1,2 +1,17 @@
-#include "stdafx.h"
 #include "proc.h"
+
+
+DWORD getProcId(const wchar_t* procName)
+{
+
+}
+
+uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName) 
+{
+
+}
+
+uintptr_t findDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>)
+{
+
+}
--- ACheat/proc.h
+++ ACheat/proc.h
@@ -1,5 +1,10 @@
 #pragma once
-class proc
-{
-};
+#include <vector>
+#include <Windows.h>
+#include <TlHelp32.h>
 
+DWORD getProcId(const wchar_t* procName);
+
+uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName);
+
+uintptr_t findDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>);
