7b2f8ad2363f9cc86e4549d57599e6221536f735
Emir Gocturk <emirgoppa@gmail.com>
2021-10-30 22:32:41 +0200

    patch memory

--- ACheat/mem.cpp
+++ ACheat/mem.cpp
@@ -3,7 +3,10 @@
 
 void mem::patchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess)
 {
-
+	DWORD oldProtect;
+	VirtualProtectEx(hProcess, dst, size, PAGE_EXECUTE_READWRITE, &oldProtect);
+	WriteProcessMemory(hProcess, dst, src, size, nullptr);
+	VirtualProtectEx(hProcess, dst, size, oldProtect, &oldProtect);
 }
 
 void mem::nopEx(BYTE* dst, unsigned int size, HANDLE hProcess)
