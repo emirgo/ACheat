f55213725eccb613c21d468a5555d321a9c49455
Emir Gocturk <emirgoppa@gmail.com>
2021-10-30 12:49:06 +0200

    desc

--- ACheat/proc.h
+++ ACheat/proc.h
@@ -5,10 +5,14 @@
 
 /*
 	Fetch 32bit processes, iterate over them 
-	until matching procName is found
+	until matching procName is found, return the process id
 */
 DWORD getProcId(const wchar_t* procName);
 
+/*
+	Fetch 32bit processes, iterate over them
+	until matching modname is found, return the base addr
+*/
 uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName);
 
 uintptr_t findDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>);
