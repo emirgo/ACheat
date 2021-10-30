21d823f272d1c8db3ef4b9b89f968d8b9ef383dd
Emir Gocturk <emirgoppa@gmail.com>
2021-10-30 12:45:43 +0200

    Desc

--- ACheat/proc.h
+++ ACheat/proc.h
@@ -3,6 +3,10 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
+/*
+	Fetch 32bit processes, iterate over them 
+	until matching procName is found
+*/
 DWORD getProcId(const wchar_t* procName);
 
 uintptr_t getModuleBaseAddress(DWORD procId, const wchar_t* modName);
