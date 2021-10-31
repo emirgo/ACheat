5224bdcad5112ac3144a6d827d80a9c30dcd39dc
Emir Gocturk <emirgoppa@gmail.com>
2021-10-31 16:59:04 +0100

    new main

--- ACheat/ACheat.cpp
+++ ACheat/ACheat.cpp
@@ -12,48 +12,23 @@ int maxAmmo = 1337;
 
 int main()
 {
-	// get procid of the target
+	HANDLE hProcess = 0;
+
+	uintptr_t moduleBase = 0, localPlayerPtr = 0, healthAddr = 0;
+	bool bHealth = false, bAmmo = false;
+
+	const int newValue = 1337;
+
 	DWORD procId = getProcId(L"ac_client.exe");
 
-	// get module base addr
-	uintptr_t moduleBase = getModuleBaseAddress(procId, L"ac_client.exe");
-
-	// get handle to process
-	HANDLE hProcess = 0;
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-
-	// resolve base addr of the pointer chain
-	uintptr_t dynamicptrBaseAddr = moduleBase + 0x109B74;
-
-	std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicptrBaseAddr << std::endl;
-
-	// resolve ammo ptr chain
-	std::vector<unsigned int> ammoOffsets = { 0x150 };
-	uintptr_t ammoAddr = findDMAAddy(hProcess, dynamicptrBaseAddr, ammoOffsets);
-
-	std::cout << "AmmoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
-
-
-	while (true)
+	if (procId)
 	{
-		// read ammo value
-		int ammoValue = 0;
-		ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-		if (ammoValue < maxAmmo)
-		{
-			do
-			{
-				// write ammo value
-				WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &maxAmmo, sizeof(maxAmmo), nullptr);
+		hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 
-				// read ammo value to confirm
-				ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-			} while (ammoValue < maxAmmo);
-		}
-		
-		Sleep(1);
+		moduleBase = getModuleBaseAddress(procId, L"ac_client.exe");
+
+		localPlayerPtr = moduleBase + 0x109B74;
 	}
-	
 
 	return 0;
 }
