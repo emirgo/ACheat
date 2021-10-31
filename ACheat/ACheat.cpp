f675bb1e4f729a1847fd275040f0847a575a302d
Emir Gocturk <emirgoppa@gmail.com>
2021-10-31 17:06:16 +0100

    input polling

--- ACheat/ACheat.cpp
+++ ACheat/ACheat.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 #include "proc.h"
-#include "UserSettings.h"
+#include "mem.h"
 
 enum Address { HEALTH, ASSAULT_RIFLE_AMMO, PISTOL_AMMO, GRENADE_AMMO };
 
@@ -28,6 +28,37 @@ int main()
 		moduleBase = getModuleBaseAddress(procId, L"ac_client.exe");
 
 		localPlayerPtr = moduleBase + 0x109B74;
+
+		healthAddr = findDMAAddy(hProcess, localPlayerPtr, { 0xF8 });
+	}
+	else
+	{
+		std::cout << "Process not found.";
+		return 0;
+	}
+
+	DWORD dwExit = 0;
+
+	while (GetExitCodeProcess(hProcess, &dwExit) && dwExit == STILL_ACTIVE)
+	{
+		if (GetAsyncKeyState(VK_DELETE) & 1)
+		{
+			bHealth = !bHealth;
+		}
+
+		if (GetAsyncKeyState(VK_INSERT) & 1)
+		{
+			bAmmo = !bAmmo;
+
+			if (bAmmo)
+			{
+				mem::patchEx((BYTE*)(moduleBase + 0x637e9), (BYTE*)"\xFF\x06", 2, hProcess);
+			}
+			else
+			{
+				mem::patchEx((BYTE*)(moduleBase + 0x637e9), (BYTE*)"\xFF\x0E", 2, hProcess);
+			}
+		}
 	}
 
 	return 0;
